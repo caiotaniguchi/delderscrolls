96a5e1844da44a13ffd07064c669113d0f5eb0ef
bluuz <pedroorez@gmail.com>
2013-11-30 21:15:30 -0200

    Delete camera.cpp

--- camera.cpp
+++
@@ -1,8 +0,0 @@
-#include "camera.h"
-#include <cmath>
-#include <GL/gl.h>		   // Open Graphics Library (OpenGL) header
-#include <GL/glut.h>	   // The GL Utility Toolkit (GLUT) Header
-#include "constants.h"
-#include <iostream>
-
-// Constructor
